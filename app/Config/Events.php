87f47a92701221985744ba1cc892fcc65e8d6691
Jorge Luiz <bjxjorge@gmail.com>
2025-11-23 14:21:16 -0300

    .

--- app/Config/Events.php
+++ app/Config/Events.php
@@ -1,55 +1,67 @@
 <?php
 
-namespace Config;
+    namespace Config;
 
-use CodeIgniter\Events\Events;
-use CodeIgniter\Exceptions\FrameworkException;
-use CodeIgniter\HotReloader\HotReloader;
+    use CodeIgniter\Events\Events;
+    use CodeIgniter\Exceptions\FrameworkException;
+    use CodeIgniter\HotReloader\HotReloader;
 
-/*
- * --------------------------------------------------------------------
- * Application Events
- * --------------------------------------------------------------------
- * Events allow you to tap into the execution of the program without
- * modifying or extending core files. This file provides a central
- * location to define your events, though they can always be added
- * at run-time, also, if needed.
- *
- * You create code that can execute by subscribing to events with
- * the 'on()' method. This accepts any form of callable, including
- * Closures, that will be executed when the event is triggered.
- *
- * Example:
- *      Events::on('create', [$myInstance, 'myMethod']);
- */
 
-Events::on('pre_system', static function (): void {
-    if (ENVIRONMENT !== 'testing') {
-        if (ini_get('zlib.output_compression')) {
-            throw FrameworkException::forEnabledZlibOutputCompression();
-        }
-
-        while (ob_get_level() > 0) {
-            ob_end_flush();
-        }
-
-        ob_start(static fn ($buffer) => $buffer);
-    }
-
-    /*
+    /**
      * --------------------------------------------------------------------
-     * Debug Toolbar Listeners.
+     * Application Events
      * --------------------------------------------------------------------
-     * If you delete, they will no longer be collected.
+     * Os eventos permitem que você acompanhe a execução do
+     * programa sem modificar ou estender os arquivos principais.
+     * Este arquivo fornece um local central para definir seus
+     * eventos, embora eles também possam ser adicionados em
+     * tempo de execução, se necessário.
+     *
+     * Você cria código que pode ser executado ao se inscrever
+     * em eventos com o método 'on()'. Este método aceita qualquer
+     * tipo de função, incluindo Closures, que será executada
+     * quando o evento for acionado.
+     *
+     * Exemplo:
+     *      Events::on('create', [$myInstance, 'myMethod']);
      */
-    if (CI_DEBUG && ! is_cli()) {
-        Events::on('DBQuery', 'CodeIgniter\Debug\Toolbar\Collectors\Database::collect');
-        service('toolbar')->respond();
-        // Hot Reload route - for framework use on the hot reloader.
-        if (ENVIRONMENT === 'development') {
-            service('routes')->get('__hot-reload', static function (): void {
-                (new HotReloader())->run();
-            });
+
+    Events::on('pre_system', static function (): void {
+        if (ENVIRONMENT !== 'testing')
+        {
+            if (ini_get('zlib.output_compression'))
+            {
+                throw FrameworkException::forEnabledZlibOutputCompression();
+            }
+
+            while (ob_get_level() > 0)
+            {
+                ob_end_flush();
+            }
+
+            ob_start(static fn ($buffer) => $buffer);
         }
-    }
-});
+
+        /**
+         * --------------------------------------------------------------------
+         * Ouvintes da barra de ferramentas de depuração.
+         * --------------------------------------------------------------------
+         * Se você excluir, eles não serão mais coletados.
+         */
+        if (CI_DEBUG && ! is_cli())
+        {
+            Events::on('DBQuery', 'CodeIgniter\Debug\Toolbar\Collectors\Database::collect');
+            service('toolbar')->respond();
+
+            /**
+             * Rota de recarregamento a quente - para uso do
+             * framework no recarregador a quente.
+             */
+            if (ENVIRONMENT === 'development')
+            {
+                service('routes')->get('__hot-reload', static function (): void {
+                    (new HotReloader())->run();
+                });
+            }
+        }
+    });

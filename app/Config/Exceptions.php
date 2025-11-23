01e5a47e6322140336881b70f0216e0551f6d8e4
Jorge Luiz <bjxjorge@gmail.com>
2025-11-23 15:02:49 -0300

    .

--- app/Config/Exceptions.php
+++ app/Config/Exceptions.php
@@ -1,106 +1,120 @@
 <?php
 
-namespace Config;
+    namespace Config;
 
-use CodeIgniter\Config\BaseConfig;
-use CodeIgniter\Debug\ExceptionHandler;
-use CodeIgniter\Debug\ExceptionHandlerInterface;
-use Psr\Log\LogLevel;
-use Throwable;
+    use CodeIgniter\Config\BaseConfig;
+    use CodeIgniter\Debug\ExceptionHandler;
+    use CodeIgniter\Debug\ExceptionHandlerInterface;
+    use Psr\Log\LogLevel;
+    use Throwable;
 
-/**
- * Setup how the exception handler works.
- */
-class Exceptions extends BaseConfig
-{
-    /**
-     * --------------------------------------------------------------------------
-     * LOG EXCEPTIONS?
-     * --------------------------------------------------------------------------
-     * If true, then exceptions will be logged
-     * through Services::Log.
-     *
-     * Default: true
-     */
-    public bool $log = true;
 
     /**
-     * --------------------------------------------------------------------------
-     * DO NOT LOG STATUS CODES
-     * --------------------------------------------------------------------------
-     * Any status codes here will NOT be logged if logging is turned on.
-     * By default, only 404 (Page Not Found) exceptions are ignored.
-     *
-     * @var list<int>
+     * Configure o funcionamento do manipulador de exceções.
      */
-    public array $ignoreCodes = [404];
-
-    /**
-     * --------------------------------------------------------------------------
-     * Error Views Path
-     * --------------------------------------------------------------------------
-     * This is the path to the directory that contains the 'cli' and 'html'
-     * directories that hold the views used to generate errors.
-     *
-     * Default: APPPATH.'Views/errors'
-     */
-    public string $errorViewPath = APPPATH . 'Views/errors';
-
-    /**
-     * --------------------------------------------------------------------------
-     * HIDE FROM DEBUG TRACE
-     * --------------------------------------------------------------------------
-     * Any data that you would like to hide from the debug trace.
-     * In order to specify 2 levels, use "/" to separate.
-     * ex. ['server', 'setup/password', 'secret_token']
-     *
-     * @var list<string>
-     */
-    public array $sensitiveDataInTrace = [];
-
-    /**
-     * --------------------------------------------------------------------------
-     * WHETHER TO THROW AN EXCEPTION ON DEPRECATED ERRORS
-     * --------------------------------------------------------------------------
-     * If set to `true`, DEPRECATED errors are only logged and no exceptions are
-     * thrown. This option also works for user deprecations.
-     */
-    public bool $logDeprecations = true;
-
-    /**
-     * --------------------------------------------------------------------------
-     * LOG LEVEL THRESHOLD FOR DEPRECATIONS
-     * --------------------------------------------------------------------------
-     * If `$logDeprecations` is set to `true`, this sets the log level
-     * to which the deprecation will be logged. This should be one of the log
-     * levels recognized by PSR-3.
-     *
-     * The related `Config\Logger::$threshold` should be adjusted, if needed,
-     * to capture logging the deprecations.
-     */
-    public string $deprecationLogLevel = LogLevel::WARNING;
-
-    /*
-     * DEFINE THE HANDLERS USED
-     * --------------------------------------------------------------------------
-     * Given the HTTP status code, returns exception handler that
-     * should be used to deal with this error. By default, it will run CodeIgniter's
-     * default handler and display the error information in the expected format
-     * for CLI, HTTP, or AJAX requests, as determined by is_cli() and the expected
-     * response format.
-     *
-     * Custom handlers can be returned if you want to handle one or more specific
-     * error codes yourself like:
-     *
-     *      if (in_array($statusCode, [400, 404, 500])) {
-     *          return new \App\Libraries\MyExceptionHandler();
-     *      }
-     *      if ($exception instanceOf PageNotFoundException) {
-     *          return new \App\Libraries\MyExceptionHandler();
-     *      }
-     */
-    public function handler(int $statusCode, Throwable $exception): ExceptionHandlerInterface
+    class Exceptions extends BaseConfig
     {
-        return new ExceptionHandler($this);
+        /**
+         * --------------------------------------------------------------------------
+         * LOG EXCEPTIONS?
+         * --------------------------------------------------------------------------
+         * Se true, as exceções serão registradas através
+         * de Services::Log.
+         *
+         * Default: true
+         */
+        public bool $log = true;
+
+        /**
+         * --------------------------------------------------------------------------
+         * NÃO REGISTRAR CÓDIGOS DE STATUS
+         * --------------------------------------------------------------------------
+         * Os códigos de status aqui apresentados NÃO serão registrados
+         * se o registro de logs estiver ativado. Por padrão, apenas as
+         * exceções 404 (Página não encontrada) são ignoradas.
+         *
+         * @var list<int>
+         */
+        public array $ignoreCodes = [404];
+
+        /**
+         * --------------------------------------------------------------------------
+         * Caminho das Exibições de Erro
+         * --------------------------------------------------------------------------
+         * Este é o caminho para o diretório que contém os
+         * diretórios 'cli' e 'html', que armazenam as
+         * visualizações usadas para gerar erros.
+         *
+         * Default: APPPATH.'Views/errors'
+         */
+        public string $errorViewPath = APPPATH . 'Views/errors';
+
+        /**
+         * --------------------------------------------------------------------------
+         * OCULTAR DO RASTREAMENTO DE DEPURAÇÃO
+         * --------------------------------------------------------------------------
+         * Quaisquer dados que você queira ocultar do rastreamento
+         * de depuração. Para especificar dois níveis, use "/" para
+         * separá-los.
+         * ex. ['server', 'setup/password', 'secret_token']
+         *
+         * @var list<string>
+         */
+        public array $sensitiveDataInTrace = [];
+
+        /**
+         * --------------------------------------------------------------------------
+         * DECIDIR SE DEVE LANÇAR UMA EXCEÇÃO EM CASO DE ERROS OBSOLETOS
+         * --------------------------------------------------------------------------
+         * Se definido como `true`, apenas os erros DEPRECATED
+         * serão registrados e nenhuma exceção será lançada. Essa
+         * opção também funciona para avisos de obsolescência feitos
+         * pelo usuário.
+         */
+        public bool $logDeprecations = true;
+
+        /**
+         * --------------------------------------------------------------------------
+         * LIMITE DE NÍVEL DE REGISTRO PARA DEPRECIAÇÕES
+         * --------------------------------------------------------------------------
+         * Se `$logDeprecations` estiver definido como `true`,
+         * isso define o nível de log no qual a depreciação será
+         * registrada. Este deve ser um dos níveis de log reconhecidos
+         * pelo PSR-3.
+         *
+         * O parâmetro `Config\Logger::$threshold` relacionado deve
+         * ser ajustado, se necessário, para capturar o registro das
+         * depreciações.
+         */
+        public string $deprecationLogLevel = LogLevel::WARNING;
+
+        /**
+         * --------------------------------------------------------------------------
+         * DEFINIR OS MANIPULADORES UTILIZADOS
+         * --------------------------------------------------------------------------
+         * Dado o código de status HTTP, retorna o manipulador de
+         * exceção que deve ser usado para lidar com esse erro. Por
+         * padrão, ele executará o manipulador padrão do CodeIgniter
+         * e exibirá as informações de erro no formato esperado para
+         * solicitações CLI, HTTP ou AJAX, conforme determinado
+         * por `is_cli()` e o formato de resposta esperado.
+         *
+         * É possível retornar manipuladores personalizados caso
+         * deseje lidar com um ou mais códigos de erro específicos,
+         * como:
+         *
+         *      if (in_array($statusCode, [400, 404, 500]))
+         *      {
+         *          return new \App\Libraries\MyExceptionHandler();
+         *      }
+         *
+         *      if ($exception instanceOf PageNotFoundException)
+         *      {
+         *          return new \App\Libraries\MyExceptionHandler();
+         *      }
+         */
+        public function handler(int $statusCode, Throwable $exception): ExceptionHandlerInterface
+        {
+            return new ExceptionHandler($this);
+        }
     }
-}

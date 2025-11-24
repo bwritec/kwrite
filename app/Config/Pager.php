1656b6970290b278ff78f0f56cda66fdb5cf3212
Jorge Luiz <bjxjorge@gmail.com>
2025-11-24 14:56:54 -0300

    .

--- app/Config/Pager.php
+++ app/Config/Pager.php
@@ -1,38 +1,42 @@
 <?php
 
-namespace Config;
+    namespace Config;
 
-use CodeIgniter\Config\BaseConfig;
+    use CodeIgniter\Config\BaseConfig;
 
-class Pager extends BaseConfig
-{
-    /**
-     * --------------------------------------------------------------------------
-     * Templates
-     * --------------------------------------------------------------------------
-     *
-     * Pagination links are rendered out using views to configure their
-     * appearance. This array contains aliases and the view names to
-     * use when rendering the links.
-     *
-     * Within each view, the Pager object will be available as $pager,
-     * and the desired group as $pagerGroup;
-     *
-     * @var array<string, string>
-     */
-    public array $templates = [
-        'default_full'   => 'CodeIgniter\Pager\Views\default_full',
-        'default_simple' => 'CodeIgniter\Pager\Views\default_simple',
-        'default_head'   => 'CodeIgniter\Pager\Views\default_head',
-        'bootstrap'      => 'App\Views\pagers\bootstrap',
-    ];
 
     /**
-     * --------------------------------------------------------------------------
-     * Items Per Page
-     * --------------------------------------------------------------------------
      *
-     * The default number of results shown in a single page.
      */
-    public int $perPage = 20;
-}
+    class Pager extends BaseConfig
+    {
+        /**
+         * --------------------------------------------------------------------------
+         * Templates
+         * --------------------------------------------------------------------------
+         *
+         * Os links de paginação são renderizados usando visualizações
+         * para configurar sua aparência. Este array contém aliases e os
+         * nomes das visualizações a serem usadas ao renderizar os links.
+         *
+         * Em cada visualização, o objeto Pager estará disponível como
+         * $pager e o grupo desejado como $pagerGroup;
+         *
+         * @var array<string, string>
+         */
+        public array $templates = [
+            'default_full'   => 'CodeIgniter\Pager\Views\default_full',
+            'default_simple' => 'CodeIgniter\Pager\Views\default_simple',
+            'default_head'   => 'CodeIgniter\Pager\Views\default_head',
+            'bootstrap'      => 'App\Views\pagers\bootstrap',
+        ];
+
+        /**
+         * --------------------------------------------------------------------------
+         * Itens por página
+         * --------------------------------------------------------------------------
+         *
+         * O número padrão de resultados exibidos em uma única página.
+         */
+        public int $perPage = 20;
+    }

955761ecd7c18d3b2241aee6a85a1e3bb415f5af
Jorge Luiz <bjxjorge@gmail.com>
2025-11-24 16:43:47 -0300

    .

--- app/Config/Session.php
+++ app/Config/Session.php
@@ -1,127 +1,138 @@
 <?php
 
-namespace Config;
+    namespace Config;
 
-use CodeIgniter\Config\BaseConfig;
-use CodeIgniter\Session\Handlers\BaseHandler;
-use CodeIgniter\Session\Handlers\FileHandler;
+    use CodeIgniter\Config\BaseConfig;
+    use CodeIgniter\Session\Handlers\BaseHandler;
+    use CodeIgniter\Session\Handlers\FileHandler;
 
-class Session extends BaseConfig
-{
-    /**
-     * --------------------------------------------------------------------------
-     * Session Driver
-     * --------------------------------------------------------------------------
-     *
-     * The session storage driver to use:
-     * - `CodeIgniter\Session\Handlers\FileHandler`
-     * - `CodeIgniter\Session\Handlers\DatabaseHandler`
-     * - `CodeIgniter\Session\Handlers\MemcachedHandler`
-     * - `CodeIgniter\Session\Handlers\RedisHandler`
-     *
-     * @var class-string<BaseHandler>
-     */
-    public string $driver = FileHandler::class;
 
     /**
-     * --------------------------------------------------------------------------
-     * Session Cookie Name
-     * --------------------------------------------------------------------------
      *
-     * The session cookie name, must contain only [0-9a-z_-] characters
      */
-    public string $cookieName = 'ci_session';
+    class Session extends BaseConfig
+    {
+        /**
+         * --------------------------------------------------------------------------
+         * Session Driver
+         * --------------------------------------------------------------------------
+         *
+         * O driver de armazenamento de sessão a ser usado:
+         * - `CodeIgniter\Session\Handlers\FileHandler`
+         * - `CodeIgniter\Session\Handlers\DatabaseHandler`
+         * - `CodeIgniter\Session\Handlers\MemcachedHandler`
+         * - `CodeIgniter\Session\Handlers\RedisHandler`
+         *
+         * @var class-string<BaseHandler>
+         */
+        public string $driver = FileHandler::class;
 
-    /**
-     * --------------------------------------------------------------------------
-     * Session Expiration
-     * --------------------------------------------------------------------------
-     *
-     * The number of SECONDS you want the session to last.
-     * Setting to 0 (zero) means expire when the browser is closed.
-     */
-    public int $expiration = 7200;
+        /**
+         * --------------------------------------------------------------------------
+         * Session Cookie Name
+         * --------------------------------------------------------------------------
+         *
+         * O nome do cookie de sessão deve conter apenas caracteres [0-9a-z_-]
+         */
+        public string $cookieName = 'ci_session';
 
-    /**
-     * --------------------------------------------------------------------------
-     * Session Save Path
-     * --------------------------------------------------------------------------
-     *
-     * The location to save sessions to and is driver dependent.
-     *
-     * For the 'files' driver, it's a path to a writable directory.
-     * WARNING: Only absolute paths are supported!
-     *
-     * For the 'database' driver, it's a table name.
-     * Please read up the manual for the format with other session drivers.
-     *
-     * IMPORTANT: You are REQUIRED to set a valid save path!
-     */
-    public string $savePath = WRITEPATH . 'session';
+        /**
+         * --------------------------------------------------------------------------
+         * Session Expiration
+         * --------------------------------------------------------------------------
+         *
+         * O número de SEGUNDOS que você deseja que a sessão dure.
+         * Definir como 0 (zero) significa que expira quando o
+         * navegador for fechado.
+         */
+        public int $expiration = 7200;
 
-    /**
-     * --------------------------------------------------------------------------
-     * Session Match IP
-     * --------------------------------------------------------------------------
-     *
-     * Whether to match the user's IP address when reading the session data.
-     *
-     * WARNING: If you're using the database driver, don't forget to update
-     *          your session table's PRIMARY KEY when changing this setting.
-     */
-    public bool $matchIP = false;
+        /**
+         * --------------------------------------------------------------------------
+         * Session Save Path
+         * --------------------------------------------------------------------------
+         *
+         * O local para salvar as sessões depende do driver.
+         *
+         * Para o driver 'files', trata-se do caminho para um diretório gravável.
+         * AVISO: Somente caminhos absolutos são suportados!
+         *
+         * Para o driver 'database', trata-se do nome de uma tabela.
+         * Por favor, leia o manual para obter informações sobre o
+         * formato com outros drivers de sessão.
+         *
+         * IMPORTANTE: É OBRIGATÓRIO definir um caminho de
+         * salvamento válido!
+         */
+        public string $savePath = WRITEPATH . 'session';
 
-    /**
-     * --------------------------------------------------------------------------
-     * Session Time to Update
-     * --------------------------------------------------------------------------
-     *
-     * How many seconds between CI regenerating the session ID.
-     */
-    public int $timeToUpdate = 300;
+        /**
+         * --------------------------------------------------------------------------
+         * Session Match IP
+         * --------------------------------------------------------------------------
+         *
+         * Indica se o endereço IP do usuário deve ser comparado
+         * ao ler os dados da sessão.
+         *
+         * AVISO: Se você estiver usando o driver de banco de
+         *        dados, não se esqueça de atualizar a CHAVE
+         *        PRIMÁRIA da sua tabela de sessão ao alterar
+         *        essa configuração.
+         */
+        public bool $matchIP = false;
 
-    /**
-     * --------------------------------------------------------------------------
-     * Session Regenerate Destroy
-     * --------------------------------------------------------------------------
-     *
-     * Whether to destroy session data associated with the old session ID
-     * when auto-regenerating the session ID. When set to FALSE, the data
-     * will be later deleted by the garbage collector.
-     */
-    public bool $regenerateDestroy = false;
+        /**
+         * --------------------------------------------------------------------------
+         * Session Time to Update
+         * --------------------------------------------------------------------------
+         *
+         * Quantos segundos se passam entre a regeneração do ID da sessão pelo CI?
+         */
+        public int $timeToUpdate = 300;
 
-    /**
-     * --------------------------------------------------------------------------
-     * Session Database Group
-     * --------------------------------------------------------------------------
-     *
-     * DB Group for the database session.
-     */
-    public ?string $DBGroup = null;
+        /**
+         * --------------------------------------------------------------------------
+         * Session Regenerate Destroy
+         * --------------------------------------------------------------------------
+         *
+         * Indica se os dados da sessão associados ao ID da sessão antiga
+         * devem ser destruídos ao regenerar automaticamente o ID da sessão.
+         * Quando definido como FALSE, os dados serão posteriormente excluídos
+         * pelo coletor de lixo.
+         */
+        public bool $regenerateDestroy = false;
 
-    /**
-     * --------------------------------------------------------------------------
-     * Lock Retry Interval (microseconds)
-     * --------------------------------------------------------------------------
-     *
-     * This is used for RedisHandler.
-     *
-     * Time (microseconds) to wait if lock cannot be acquired.
-     * The default is 100,000 microseconds (= 0.1 seconds).
-     */
-    public int $lockRetryInterval = 100_000;
+        /**
+         * --------------------------------------------------------------------------
+         * Session Database Group
+         * --------------------------------------------------------------------------
+         *
+         * Grupo DB para a sessão do banco de dados.
+         */
+        public ?string $DBGroup = null;
 
-    /**
-     * --------------------------------------------------------------------------
-     * Lock Max Retries
-     * --------------------------------------------------------------------------
-     *
-     * This is used for RedisHandler.
-     *
-     * Maximum number of lock acquisition attempts.
-     * The default is 300 times. That is lock timeout is about 30 (0.1 * 300)
-     * seconds.
-     */
-    public int $lockMaxRetries = 300;
-}
+        /**
+         * --------------------------------------------------------------------------
+         * Lock Retry Interval (microseconds)
+         * --------------------------------------------------------------------------
+         *
+         * Isso é usado para o RedisHandler.
+         *
+         * Time (microseconds) aguardar caso o bloqueio não possa ser obtido.
+         * O padrão é 100,000 microseconds (= 0.1 seconds).
+         */
+        public int $lockRetryInterval = 100_000;
+
+        /**
+         * --------------------------------------------------------------------------
+         * Lock Max Retries
+         * --------------------------------------------------------------------------
+         *
+         * Isto é usado para RedisHandler.
+         *
+         * Número máximo de tentativas de aquisição de bloqueio.
+         * O valor padrão é 300 vezes. Ou seja, o tempo limite de bloqueio
+         * é de aproximadamente 30 (0.1 * 300) segundos.
+         */
+        public int $lockMaxRetries = 300;
+    }

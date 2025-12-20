f190606bed8dd3761fd867ed0264dd59f6f5b430
Jorge Luiz <bjxjorge@gmail.com>
2025-12-19 23:31:48 -0300

    .

--- public/index.php
+++ public/index.php
@@ -1,59 +1,88 @@
 <?php
 
-use CodeIgniter\Boot;
-use Config\Paths;
+    use CodeIgniter\Boot;
+    use Config\Paths;
 
-/*
- *---------------------------------------------------------------
- * CHECK PHP VERSION
- *---------------------------------------------------------------
- */
 
-$minPhpVersion = '8.1'; // If you update this, don't forget to update `spark`.
-if (version_compare(PHP_VERSION, $minPhpVersion, '<')) {
-    $message = sprintf(
-        'Your PHP version must be %s or higher to run CodeIgniter. Current version: %s',
-        $minPhpVersion,
-        PHP_VERSION,
-    );
+    /**
+     *---------------------------------------------------------------
+     * CHECK PHP VERSION
+     *---------------------------------------------------------------
+     */
 
-    header('HTTP/1.1 503 Service Unavailable.', true, 503);
-    echo $message;
+    /**
+     * If you update this, don't forget to update `spark`.
+     */
+    $minPhpVersion = '8.1';
 
-    exit(1);
-}
+    if (version_compare(PHP_VERSION, $minPhpVersion, '<'))
+    {
+        $message = sprintf(
+            'Your PHP version must be %s or higher to run CodeIgniter. Current version: %s',
+            $minPhpVersion,
+            PHP_VERSION,
+        );
 
-/*
- *---------------------------------------------------------------
- * SET THE CURRENT DIRECTORY
- *---------------------------------------------------------------
- */
+        header('HTTP/1.1 503 Service Unavailable.', true, 503);
+        echo $message;
 
-// Path to the front controller (this file)
-define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
+        exit(1);
+    }
 
-// Ensure the current directory is pointing to the front controller's directory
-if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
-    chdir(FCPATH);
-}
+    /**
+     * Caminho do .env (ajuste se necessário)
+     */
+    $envPath = dirname(__DIR__) . '/.env';
 
-/*
- *---------------------------------------------------------------
- * BOOTSTRAP THE APPLICATION
- *---------------------------------------------------------------
- * This process sets up the path constants, loads and registers
- * our autoloader, along with Composer's, loads our constants
- * and fires up an environment-specific bootstrapping.
- */
+    /**
+     * Se não existir .env, redireciona para install.php
+     */
+    if (!file_exists($envPath))
+    {
+        header('Location: install.php');
+        exit;
+    }
 
-// LOAD OUR PATHS CONFIG FILE
-// This is the line that might need to be changed, depending on your folder structure.
-require FCPATH . '../app/Config/Paths.php';
-// ^^^ Change this line if you move your application folder
+    /**
+     *---------------------------------------------------------------
+     * SET THE CURRENT DIRECTORY
+     *---------------------------------------------------------------
+     */
 
-$paths = new Paths();
+    /**
+     * Path to the front controller (this file)
+     */
+    define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
 
-// LOAD THE FRAMEWORK BOOTSTRAP FILE
-require $paths->systemDirectory . '/Boot.php';
+    /**
+     * Ensure the current directory is pointing to the front controller's directory
+     */
+    if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH)
+    {
+        chdir(FCPATH);
+    }
 
-exit(Boot::bootWeb($paths));
+    /**
+     *---------------------------------------------------------------
+     * BOOTSTRAP THE APPLICATION
+     *---------------------------------------------------------------
+     * This process sets up the path constants, loads and registers
+     * our autoloader, along with Composer's, loads our constants
+     * and fires up an environment-specific bootstrapping.
+     */
+
+    /**
+     * LOAD OUR PATHS CONFIG FILE
+     * This is the line that might need to be changed, depending on your folder structure.
+     */
+    require FCPATH . '../app/Config/Paths.php';
+    // ^^^ Change this line if you move your application folder
+
+    $paths = new Paths();
+
+    /**
+     * LOAD THE FRAMEWORK BOOTSTRAP FILE
+     */
+    require $paths->systemDirectory . '/Boot.php';
+
+    exit(Boot::bootWeb($paths));
---
+++ public/install.php
@@ -0,0 +1,33 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+    <!--
+      - Charset.
+     -->
+    <meta charset="utf-8">
+
+    <!--
+      - Viewport.
+     -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--
+      - Título.
+     -->
+    <title>Instalador</title>
+
+</head>
+<body>
+
+    <h1>
+        Instalar
+    </h1>
+
+</body>
+</html>

a40f13f19740218d069969c3a9c6790cfae833b4
Jorge Luiz <bjxjorge@gmail.com>
2026-04-13 12:34:12 -0300

    .

--- app/Views/system/register.php
+++ app/Views/system/register.php
@@ -58,44 +58,101 @@
                     <?php endif; ?>
                 </div>
 
-
-
                 <div class="input-group mb-3">
-                  <input type="email" class="form-control" placeholder="Email">
-                  <div class="input-group-append">
-                    <div class="input-group-text">
-                      <span class="fas fa-envelope"></span>
+                    <input type="email" class="form-control <?= isset($errors['email']) ? 'is-invalid' : '' ?>" name="email" value="<?= old('email') ?>" placeholder="Email">
+
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
+                        </div>
                     </div>
-                  </div>
+
+                    <?php if (isset($errors['email'])): ?>
+                        <div class="invalid-feedback">
+                            <?= esc($errors['email']) ?>
+                        </div>
+                    <?php endif; ?>
                 </div>
 
                 <div class="input-group mb-3">
-                  <input type="password" class="form-control" placeholder="Password">
-                  <div class="input-group-append">
-                    <div class="input-group-text">
-                      <span class="fas fa-lock"></span>
+                    <input type="text" id="cpf" class="form-control <?= isset($errors['cpf']) ? 'is-invalid' : '' ?>" name="cpf" maxlength="14" value="<?= old('cpf') ?>" placeholder="CPF">
+
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <i class="fas fa-passport"></i>
+                        </div>
                     </div>
-                  </div>
+
+                    <?php if (isset($errors['cpf'])): ?>
+                        <div class="invalid-feedback">
+                            <?= esc($errors['cpf']) ?>
+                        </div>
+                    <?php endif; ?>
                 </div>
+
                 <div class="input-group mb-3">
-                  <input type="password" class="form-control" placeholder="Retype password">
-                  <div class="input-group-append">
-                    <div class="input-group-text">
-                      <span class="fas fa-lock"></span>
+                    <input type="password" class="form-control <?= isset($errors['password']) ? 'is-invalid' : '' ?>" name="password" placeholder="Senha">
+
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                        </div>
                     </div>
-                  </div>
+
+                    <?php if (isset($errors['password'])): ?>
+                        <div class="invalid-feedback">
+                            <?= esc($errors['password']) ?>
+                        </div>
+                    <?php endif; ?>
                 </div>
+
                 <div class="row">
-                  <div class="col-4">
-                    <button type="submit" class="btn btn-primary btn-block">Register</button>
-                  </div>
+                    <div class="col-4">
+                        <button type="submit" class="btn btn-primary btn-block">
+                            Cadastrar
+                        </button>
+                    </div>
                 </div>
             </form>
 
-            <a href="login.html" class="text-center">
-                I already have a membership
+            <a href="<?= base_url() ?>index.php/login" class="text-center">
+                Já tenho uma conta
             </a>
         </div>
     </div>
 </div>
+
+
+<!--
+  - Script
+ -->
+<script type="text/javascript">
+    /**
+     * Máscara de CPF em tempo real.
+     */
+    document.getElementById('cpf').addEventListener('input', function (e)
+    {
+        /**
+         * Remove tudo que não for número
+         */
+        let value = e.target.value.replace(/\D/g, '');
+
+        /**
+         * Limita a 11 dígitos.
+         */
+        if (value.length > 11)
+        {
+            value = value.slice(0, 11);
+        }
+
+        /**
+         * aplica a máscara.
+         */
+        value = value.replace(/(\d{3})(\d)/, '$1.$2');
+        value = value.replace(/(\d{3})(\d)/, '$1.$2');
+        value = value.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+
+        e.target.value = value;
+    });
+</script>
 <?= $this->endSection() ?>

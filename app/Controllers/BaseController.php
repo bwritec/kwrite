88e8d83c4bc63d9c5156e7664f66b8c2a223cc08
Jorge Luiz <bjxjorge@gmail.com>
2026-04-27 12:22:00 -0300

    .

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -1,109 +1,124 @@
 <?php
 
-namespace App\Controllers;
+    namespace App\Controllers;
 
-use CodeIgniter\Controller;
-use CodeIgniter\HTTP\CLIRequest;
-use CodeIgniter\HTTP\IncomingRequest;
-use CodeIgniter\HTTP\RequestInterface;
-use CodeIgniter\HTTP\ResponseInterface;
-use Psr\Log\LoggerInterface;
+    use CodeIgniter\Controller;
+    use CodeIgniter\HTTP\CLIRequest;
+    use CodeIgniter\HTTP\IncomingRequest;
+    use CodeIgniter\HTTP\RequestInterface;
+    use CodeIgniter\HTTP\ResponseInterface;
+    use Psr\Log\LoggerInterface;
 
-use App\Models\CategoryModel;
-use App\Models\LinkModel;
+    use App\Models\CategoryModel;
+    use App\Models\LinkModel;
 
-/**
- * Class BaseController
- *
- * BaseController provides a convenient place for loading components
- * and performing functions that are needed by all your controllers.
- * Extend this class in any new controllers:
- *     class Home extends BaseController
- *
- * For security be sure to declare any new methods as protected or private.
- */
-abstract class BaseController extends Controller
-{
-    /**
-     * A lista de categorias do site.
-     */
-    protected $global_categories;
 
     /**
-     * A lista de links do site.
-     */
-    protected $global_links;
-
-    /**
-     * Instance of the main Request object.
+     * Class BaseController
      *
-     * @var CLIRequest|IncomingRequest
-     */
-    protected $request;
-
-    /**
-     * An array of helpers to be loaded automatically upon
-     * class instantiation. These helpers will be available
-     * to all other controllers that extend BaseController.
+     * BaseController Oferece um local conveniente para carregar
+     * componentes e executar funções necessárias para todos os
+     * seus controllers.
      *
-     * @var list<string>
+     * Estenda esta classe em qualquer novo controllers:
+     *     class Home extends BaseController
+     *
+     * Por questões de segurança, certifique-se de declarar
+     * quaisquer novos métodos como protegidos ou privados.
      */
-    protected $helpers = [];
-
-    /**
-     * Be sure to declare properties for any property fetch you initialized.
-     * The creation of dynamic property is deprecated in PHP 8.2.
-     */
-    // protected $session;
-
-    /**
-     * @return void
-     */
-    public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
+    abstract class BaseController extends Controller
     {
-        // Do Not Edit This Line
-        parent::initController($request, $response, $logger);
+        /**
+         * A lista de categorias do site.
+         */
+        protected $global_categories;
 
         /**
-         * Chama o helper
+         * A lista de links do site.
          */
-        helper('category');
-
-        // Preload any models, libraries, etc, here.
-
-        // E.g.: $this->session = service('session');
+        protected $global_links;
 
         /**
-         * Adicionar links ao menu do site.
+         * Instância do objeto Request principal.
+         *
+         * @var CLIRequest|IncomingRequest
          */
-        $linkModel = new LinkModel();
-        $this->global_links = $linkModel->orderBy('id', 'ASC')->findAll();
+        protected $request;
 
         /**
-         * Adicionar categorias ao menu dropdown
-         * do site.
+         * Um conjunto de funções auxiliares a serem carregadas
+         * automaticamente na instanciação da classe. Essas
+         * funções auxiliares estarão disponíveis para todos os
+         * outros controllers que estendem BaseController.
+         *
+         * @var list<string>
          */
-        $categories = new CategoryModel();
-        $allCategories = $categories->orderBy('name', 'ASC')->findAll();
+        protected $helpers = [];
 
         /**
-         * monta árvore de categorias (pai > filhos)
+         * Certifique-se de declarar as propriedades para
+         * qualquer busca de propriedade que você tenha
+         * inicializado.
+         * 
+         * A criação de propriedades dinâmicas foi descontinuada
+         * no PHP 8.2.
          */
-        $this->global_categories = build_category_tree($allCategories);
+        // protected $session;
 
         /**
-         * --- disponibiliza 'categories' para todas as views ---
-         * use o service 'renderer' em vez de view() sem parâmetros
+         * @return void
          */
-        $renderer = \Config\Services::renderer();
+        public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
+        {
+            /**
+             * Não edite esta linha
+             */
+            parent::initController($request, $response, $logger);
 
-        /**
-         * setData mantém/mescla dados compartilhados para as views
-         * (setData espera um array associativo)
-         */
-        $renderer->setData([
-            'global_categories' => $this->global_categories,
-            'global_links' => $this->global_links
-        ]);
+            /**
+             * Chama o helper
+             */
+            helper('category');
+
+            /**
+             * Pré-carregue aqui quaisquer modelos, bibliotecas, etc.
+             */
+
+            /**
+             * Exemplo: $this->session = service('session');
+             */
+
+            /**
+             * Adicionar links ao menu do site.
+             */
+            $linkModel = new LinkModel();
+            $this->global_links = $linkModel->orderBy('id', 'ASC')->findAll();
+
+            /**
+             * Adicionar categorias ao menu dropdown
+             * do site.
+             */
+            $categories = new CategoryModel();
+            $allCategories = $categories->orderBy('name', 'ASC')->findAll();
+
+            /**
+             * monta árvore de categorias (pai > filhos)
+             */
+            $this->global_categories = build_category_tree($allCategories);
+
+            /**
+             * --- disponibiliza 'categories' para todas as views ---
+             * use o service 'renderer' em vez de view() sem parâmetros
+             */
+            $renderer = \Config\Services::renderer();
+
+            /**
+             * setData mantém/mescla dados compartilhados para as views
+             * (setData espera um array associativo)
+             */
+            $renderer->setData([
+                'global_categories' => $this->global_categories,
+                'global_links' => $this->global_links
+            ]);
+        }
     }
-}
